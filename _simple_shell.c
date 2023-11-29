24a1f77134bfce260112a0a080510c49233748f7
Tatenda35 <tatndor@gmail.com>
2023-11-29 11:56:51 -0800

    algorithm flow for task 1

---
+++ _simple_shell.c
@@ -0,0 +1,17 @@
+/* Step1 file start */
+Open file
+
+/* Step2 shell prompt */
+ask user for input word
+
+/*Input command, check builtin or executable 
+ * Built in ecldue or display shell promt
+ */
+check system call for matching word
+
+/* Print new line and prompt*/
+if successfull print outpt else take user back to prompt
+
+/*End of file*/
+
+
