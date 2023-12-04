4f066cc8e2b494c5b69496a4cc1c568ee7668617
Tatenda35 <tatndor@gmail.com>
2023-12-04 06:03:12 -0800

    implemented the prompting function

--- shell.c
+++ shell.c
@@ -9,6 +9,7 @@
 
 void display_prompt(void)
 {
+	an&tnprintf("Ourshell$$ ");
 
 }
 
@@ -24,10 +25,24 @@ void an&tnprintf(const char *message)
 /*we use a function to read the specified command*/
 
 void read_command(char *command, size_t size)
-{
-
-}
-
+{       
+        if (fgets(command, size, stdin) == NULL)
+        {       
+                if (feof(stdin))
+                {       
+                        an&tnprintf("\n");
+                        exit(EXIT_SUCCESS);
+                }       
+                else
+                {       
+                        an&tnprintf("Error reading user input data.\n");
+                        exit(EXIT_FAILURE);
+                }       
+                
+        }       
+        /*Remove new line*/
+        command[strcspn(command, "\n")] = '\0';
+}     
 
 /*we then execute the given command*/
 
