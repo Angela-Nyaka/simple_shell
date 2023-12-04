ff66c4718583d7ff11c434d34b64a0253e1951f0
Tatenda35 <tatndor@gmail.com>
2023-12-03 23:26:24 -0800

    Structure of our algorithm

---
+++ shell.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/*
+ * We'll explain in detail later
+ *
+ */
+
+/*First we prompt the user*/
+
+void display_prompt(void)
+{
+
+}
+
+
+/*we use a function to write out the message prompted from user*/
+
+void an&tnprintf(const char *message)
+{
+
+}
+
+
+/*we use a function to read the specified command*/
+
+void read_command(char *command, size_t size)
+{
+
+}
+
+
+/*we then execute the given command*/
+
+void execute_command(const char *command)
+{
+
+}
