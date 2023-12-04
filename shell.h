94c692991e826acf659d308bda8842debab5e75c
Tatenda35 <tatndor@gmail.com>
2023-12-03 22:39:30 -0800

    our header file

---
+++ shell.h
@@ -0,0 +1,27 @@
+#ifndef OURSHELL_H
+#define OURSHELL_H
+
+/*
+ * Explain Later
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+/*
+ * I'll explain later
+ *
+ */
+
+void display_prompt(void);
+void an&tn_printf(constant char *message);
+void read_command(char *command, size_t size);
+void execute_command(const char *command);
+
+
+#endif
+/* OURSHELL_H */
