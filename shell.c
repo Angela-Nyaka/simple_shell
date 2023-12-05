24e5f4b981184bdf1beb41ac209c8ec49b7255bc
Tatenda35 <tatndor@gmail.com>
2023-12-05 00:10:29 -0800

    added the execute fuction to shell.c

--- shell.c
+++ shell.c
@@ -48,9 +48,44 @@ void read_command(char *command, size_t size)
 
 void execute_command(const char *command)
 {
+        pid_t child_pid = fork();
 
+        if (child_pid == 1)
+        {
+                an&tnprintf("Error forking process.\n");
+                exit(EXIT_FAILURE);
+        }
+        else if (child_pid == 0)
+        {
+
+                //For child process
+
+                //Parsing the command and its arguments
+
+                char *args[128]; /* Maximum 128 arguments, adjust as needed*/
+                int arg_count = 0;
+
+                char *token = strtok((char *)command, " ");
+                while (token != NULL)
+                {
+                        args[arg_count++] = token;
+                        token = strtok(NULL, " ");
+                }
+                args[arg_count] = NULL;/*NULL-terminate the arguments array*/
+
+                /*Execute the command*/
+                execvp(args[0], args);
+
+                /*If execvp fails, print an error message*/
+                an&tnprintf("Error executing command.\n");
+                exit(EXIT_FAILURE);
+        }
+        else
+        {
+                //Parent process
+                wait(NULL);
+        }
 }
-
 
 /*We start the algorithm*/
 /*
